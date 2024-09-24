fcbcd9d0bb10d8afef4a8f34424d112a43bf47b3
ErlebnisW <787368748w@gmail.com>
2024-09-24 08:14:17 +0800

    add eval sript

--- useful_code/eval.sh
+++ useful_code/eval.sh
@@ -1,4 +1,6 @@
-export export HF_ENDPOINT="https://hf-mirror.com"
+export HF_ENDPOINT="https://hf-mirror.com"
 
-python eval_reward_bench_pm.py
+# python eval_reward_bench_pm.py
+
+python eval_reward_bench_bt.py --reward_name_or_path /data/wmz_workspace/RLHF-Reward-Modeling/bradley-terry-rm/bt_models/gemma2b_rm/last_checkpoint
 
